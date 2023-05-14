af98df132a737d46be34308c892f1d9b723bd285
Ajitesh-coder <74655785+Ajitesh-coder@users.noreply.github.com>
2023-05-15 00:24:28 +0530

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -13,7 +13,7 @@ res.status(200)
 })
     }
     catch(err){
-        res.status(500).json({success:true,message:"internal server error"});
+        res.status(500).json({success:true,message:"Please fill all the required details"});
     }
 }
 
